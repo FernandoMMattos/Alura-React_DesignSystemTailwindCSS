bc7ad67a2adc4b9b16db573b2cf62f61a2e2956c
Etcloide <etcloide@gmail.com>
2024-07-20 01:12:36 -0300

    full project

---
+++ .storybook/main.ts
@@ -0,0 +1,28 @@
+import type { StorybookConfig } from "@storybook/nextjs";
+const config: StorybookConfig = {
+  stories: [
+    "../components/**/*.mdx",
+    "../components/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
+
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@chromatic-com/storybook",
+  ],
+
+  staticDirs: ["../public"],
+
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
+  },
+
+  docs: {},
+
+  typescript: {
+    reactDocgen: "react-docgen-typescript",
+  },
+};
+export default config;
---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="h-screen mx-auto my-0 w-4/5 flex justify-center">
+      <div className="w-2/4 flex flex-col gap-1 justify-start"></div>
+    </div>
+  );
+};
+
+export default App;
